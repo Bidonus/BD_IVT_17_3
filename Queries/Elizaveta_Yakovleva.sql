9bd0625ae344557fcc96964d316be9277d65b3f7
zumaka <elise.333@yandex.ru>
2018-09-19 15:59:00 +0300

    added text

--- Queries/Elizaveta_Yakovleva.sql
+++ Queries/Elizaveta_Yakovleva.sql
@@ -1,5 +1,5 @@
-select * from AdventureWorksLT.SalesLT.Customer;
+select * from Customer;
 
-select Title, FirstName, MiddleName, LastName, Suffix from AdventureWorksLT.SalesLT.Customer;
+select Title, FirstName, MiddleName, LastName, Suffix from Customer;
 
-select SalesPerson, (Title +' '+ LastName) as CustomerName, Phone from AdventureWorksLT.SalesLT.Customer;
+select SalesPerson, (Title +' '+ LastName) as CustomerName, Phone from Customer;
